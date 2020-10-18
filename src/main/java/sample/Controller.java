2f1a26f36b7d6481206f5ddce0203e398361af93
Radu Guran <radug314@gmail.com>
2020-10-18 22:52:47 +0300

    Added some design

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,20 +1,5 @@
 package sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-
 public class Controller {
 
-
-    @FXML
-    private Button button;
-    @FXML
-    private Label label;
-
-    @FXML
-    private void initialize() {
-        button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> label.setText("Clicked!!!"));
-    }
 }
