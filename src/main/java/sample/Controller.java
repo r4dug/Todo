237bacf4b0c26d2322d3600b65f95258608c91d2
Radu Guran <radug314@gmail.com>
2020-10-18 22:27:10 +0300

    Solved a bunch of errors to make project runnable

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+
+    @FXML
+    private Button button;
+    @FXML
+    private Label label;
+
+    @FXML
+    private void initialize() {
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> label.setText("Clicked!!!"));
+    }
+}
